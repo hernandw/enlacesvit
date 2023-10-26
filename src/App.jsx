0a154e9377f242347013f7a31c76f1914f8dd886
Williams Hernández <hernandw@gmail.com>
2023-10-26 12:10:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MyRoutes from "./components/MyRoutes/MyRoutes"
+import NavBar from "./components/NavBar"
+
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <MyRoutes />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+
+import MyRoutes from "./MyRoutes/MyRoutes";
+
+const NavBar = () => {
+  return (
+    <>
+     
+          <div>
+              <div>
+                  Logo
+              </div>
+              <ul>
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/about">About</a></li>
+                  <li><a href="/contact">Contact</a></li>
+              </ul>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
